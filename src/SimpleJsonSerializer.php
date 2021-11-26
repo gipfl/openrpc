99bc739a91982ce6d3b73c5bbf63a63b73d4b997
Thomas Gelf <thomas@gelf.net>
2021-11-26 10:39:07 +0100

    SimpleJsonSerializer: PHP 8.1

--- src/SimpleJsonSerializer.php
+++ src/SimpleJsonSerializer.php
@@ -4,6 +4,7 @@ namespace gipfl\OpenRpc;
 
 trait SimpleJsonSerializer
 {
+    #[\ReturnTypeWillChange]
     public function jsonSerialize()
     {
         return (object) array_filter(get_object_vars($this), function ($value) {
